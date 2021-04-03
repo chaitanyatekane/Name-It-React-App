8f886a5628473aa2b1af26e1d2ceb3677bb39ae6
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-04-03 16:38:32 +0530

    Added Project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import Header from '../Header/Header';
 import SearchBox from '../SearchBox/SearchBox';
 import './App.css';
+
+const name = require('@rstacruz/startup-name-generator');
 
 class App extends React.Component {
     constructor(){
@@ -9,20 +12,26 @@ class App extends React.Component {
 
         this.state = {
             headerText: 'Name It!',
-            headerExpanded: true
+            headerExpanded: true,
+            suggestedNames : [],
         };
     }
 
     handleInputChange = (inputText) => {
-        this.setState({ headerExpanded: !inputText });
+        this.setState({ 
+            headerExpanded: !inputText, 
+            suggestedNames: inputText ? name(inputText) : [],
+        });
     };
 
 
     render() {
+
         return (
             <div>
                 <Header headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText} />
                 <SearchBox onInputChange={this.handleInputChange} />
+                <ResultsContainer suggestedNames={this.state.suggestedNames} />
             </div>
         );
     }
